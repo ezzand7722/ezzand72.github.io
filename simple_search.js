3acfea080957bc2ec976a626277af1edae18d8af
ezzand7722 <ezzand72@gmail.com>
2024-12-17 23:43:02 +0300

    Delete simple_search.js

--- simple_search.js
+++
@@ -1,4 +0,0 @@
-function searchYouTube(searchQuery) {
-    const searchUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`;
-    window.open(searchUrl, '_blank');
-} 
